1c7a6cc1c62c446443fbc3268c220daadc036168
ygrekdi <ygrekdi@gmail.com>
2021-12-05 10:36:11 +0300

    HW03 - add initialize and populate meals table in DB, implement JdbcMealRepository

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -21,5 +22,16 @@ CREATE TABLE user_roles
     user_id INTEGER NOT NULL,
     role    VARCHAR,
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    datetime    TIMESTAMP NOT NULL,
+    description VARCHAR   NOT NULL,
+    calories    INTEGER   NOT NULL,
+    user_id     INTEGER   NOT NULL,
+    CONSTRAINT meals_datetime_idx UNIQUE (user_id, datetime),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
+DELETE
+FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +13,14 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id)
+VALUES ('2020-01-30 10:00', 'Завтрак', 500, 100000),
+       ('2020-01-30 13:00', 'Обед', 1000, 100000),
+       ('2020-01-30 20:00', 'Ужин', 500, 100000),
+       ('2020-01-31 00:00', 'Еда на граничное значение', 100, 100000),
+       ('2020-01-31 10:00', 'Завтрак', 1000, 100000),
+       ('2020-01-31 13:00', 'Обед', 500, 100000),
+       ('2020-01-31 20:00', 'Ужин', 410, 100000),
+       ('2015-06-01 14:00', 'Админ ланч', 510, 100001),
+       ('2025-01-31 21:00', 'Админ ужин', 1500, 100001);
